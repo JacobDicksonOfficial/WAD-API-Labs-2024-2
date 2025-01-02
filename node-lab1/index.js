a047ca4fe215770574219309eb495ebeef7a646b
Jacob Dickson <jjacobdofficial@gmail.com>
2025-01-02 20:41:55 +0000

    node-lab1,  greeting module added

--- node-lab1/index.js
+++ node-lab1/index.js
@@ -1,22 +1,37 @@
 // Load the http module to create an HTTP server.
 import http from 'http';
 import dotenv from 'dotenv';
+import greeting from './greeting.js';
 
-// Load environment variables from .env
+// Load environment variables
 dotenv.config();
 
-// Read the port from environment variables
+// Get the port from .env
 const port = process.env.PORT;
 
-// Configure our HTTP server to respond with Hello World to all requests.
+// Configure our HTTP server to respond with a language-specific message
 const server = http.createServer((req, res) => {
-  res.writeHead(200);
-  res.end('Hello WAD2!');
+  let lang = req.headers['accept-language'];
+  const defaultLang = 'en';
 
+  // If the requested language is not available, use the default
+  if (!greeting[lang]) lang = defaultLang;
+
+  const response = {
+    lang: lang,
+    message: greeting[lang],
+  };
+
+  // Send response headers and body
+  res.writeHead(200, {
+    'Content-Type': 'text/plain',
+    'Content-Language': response.lang,
+  });
+  res.end(response.message);
 });
 
 // Start the server
 server.listen(port);
 
-// Log a friendly message to the terminal
+// Log the server status
 console.log(`Server running at ${port}`);
