f1def473338f705b875e0d2c1bcea86fd44a20c6
Jacob Dickson <jjacobdofficial@gmail.com>
2025-01-03 17:20:32 +0000

    Integration Lab - Get Movies from API

--- reactApp/src/api/movies-api.js
+++ reactApp/src/api/movies-api.js
@@ -1,10 +1,3 @@
-export const getMovies = async () => {
-    const response = await  fetch(
-      `https://api.themoviedb.org/3/discover/movie?api_key=c1gfdg225ecfffffffff3612d8c1bcb4bcb&language=en-US&include_adult=false&page=1`
-    )
-    return response.json()
-  };
-
 export const login = async (username, password) => {
   const response = await fetch('http://localhost:8080/api/users', {
     headers: {
@@ -25,4 +18,14 @@ export const signup = async (username, password) => {
   body: JSON.stringify({ username: username, password: password })
 });
 return response.json();
+};
+
+export const getMovies = async () => {
+  const response = await fetch(
+    'http://localhost:8080/api/movies', {
+    headers: {
+      'Authorization': window.localStorage.getItem('token')
+    }
+  });
+  return response.json();
 };
