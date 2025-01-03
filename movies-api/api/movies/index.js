68ebe7c0d6beeec47ce71d490eb066423a10b35c
Jacob Dickson <jjacobdofficial@gmail.com>
2025-01-03 15:48:16 +0000

    Upcoming movies from TMDB

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -1,6 +1,8 @@
 import movieModel from './movieModel';
 import asyncHandler from 'express-async-handler';
 import express from 'express';
+import { getUpcomingMovies } from '../tmdb-api';
+
 
 const router = express.Router();
 
@@ -23,5 +25,11 @@ router.get('/:id', asyncHandler(async (req, res) => {
         });
     }
 }));
+
+router.get('/tmdb/upcoming', asyncHandler(async (req, res) => {
+    const upcomingMovies = await getUpcomingMovies();
+    res.status(200).json(upcomingMovies);
+}));
+
 
 export default router;
