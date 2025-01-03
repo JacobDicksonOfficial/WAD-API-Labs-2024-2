8abda98df23aa1ec0b21a090c6fc6ff43e127f98
Jacob Dickson <jjacobdofficial@gmail.com>
2025-01-03 15:52:46 +0000

    Salt and Hash

--- movies-api/api/users/userModel.js
+++ movies-api/api/users/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt'; // Import bcrypt for hashing
 
 const Schema = mongoose.Schema;
 
@@ -13,6 +14,32 @@ const UserSchema = new Schema({
       },
       message: props => `${props.value} is not a valid password! Password must be at least 8 characters long and include one letter, one number, and one special character.`
     }
+  }
+});
+
+// Add an instance method to compare passwords
+UserSchema.methods.comparePassword = async function (passw) {
+  return await bcrypt.compare(passw, this.password);
+};
+
+// Add a static method to find users by username
+UserSchema.statics.findByUserName = function (username) {
+  return this.findOne({ username: username });
+};
+
+// Add a pre-save hook to hash the password before saving
+UserSchema.pre('save', async function (next) {
+  const saltRounds = 10; // Number of salt rounds for bcrypt
+  if (this.isModified('password') || this.isNew) {
+    try {
+      const hash = await bcrypt.hash(this.password, saltRounds);
+      this.password = hash; // Replace plain text password with hashed password
+      next();
+    } catch (error) {
+      next(error); // Pass error to next middleware
+    }
+  } else {
+    next();
   }
 });
 
