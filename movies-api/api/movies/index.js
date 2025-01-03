62e643918c609be7e6ddf603abfc853072cc8e8b
Jacob Dickson <jjacobdofficial@gmail.com>
2025-01-03 16:33:14 +0000

    Authentication lab - exercise (genres)

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -2,6 +2,8 @@ import movieModel from './movieModel';
 import asyncHandler from 'express-async-handler';
 import express from 'express';
 import { getUpcomingMovies } from '../tmdb-api';
+import { getMovieGenres } from '../tmdb-api';
+
 
 
 const router = express.Router();
@@ -49,6 +51,12 @@ router.get('/tmdb/upcoming', asyncHandler(async (req, res) => {
     const upcomingMovies = await getUpcomingMovies();
     res.status(200).json(upcomingMovies);
 }));
+
+router.get('/tmdb/genres', asyncHandler(async (req, res) => {
+    const genres = await getMovieGenres();
+    res.status(200).json(genres);
+}));
+
 
 
 export default router;
